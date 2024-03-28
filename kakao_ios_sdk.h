339fff77be415cf091229e18f428f8fc026b5f2c
wanny.park <wanny.park@pandora.tv>
2024-03-28 10:52:43 +0900

    carthage 테스트 배포

---
+++ kakao_ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  kakao_ios_sdk.h
+//  kakao-ios-sdk
+//
+//  Created by manaes on 3/28/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kakao_ios_sdk.
+FOUNDATION_EXPORT double kakao_ios_sdkVersionNumber;
+
+//! Project version string for kakao_ios_sdk.
+FOUNDATION_EXPORT const unsigned char kakao_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kakao_ios_sdk/PublicHeader.h>
+
+
